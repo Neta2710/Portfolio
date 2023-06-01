fdd4f552a7cb2aa8247cd44f205cf5fce2170246
Neta <chetritnetanael@gmail.com>
2023-06-01 15:56:53 +0200

    add modif

---
+++ src/components/TopHeader.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import './css/TopHeader.css';
+
+export const TopHeader = () => {
+  return (
+    <div>
+     <h1>Test</h1>
+     <h1>Test</h1>
+     <h1>Test</h1>
+     <h1>Test</h1>
+    </div>
+  )
+}
