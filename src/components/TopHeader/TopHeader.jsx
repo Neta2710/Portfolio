3b116016f9ef0b8315ad5781f738092e4c3051f2
Neta <chetritnetanael@gmail.com>
2023-06-01 17:31:41 +0200

    update neta v2

--- src/components/TopHeader/TopHeader.jsx
+++ src/components/TopHeader/TopHeader.jsx
@@ -5,8 +5,25 @@ import logo from '../../assets/logo/logo.png';
 
 export const TopHeader = () => {
   return (
-    <div>
-     <h1>Test</h1>
-    </div>
-  )
-}
+    <nav id="navHeader">
+      <span id="principalSpan">
+        <span id="firstSpan"><img src={logo} width="25" height="25" id="logo"/> CacheTech.© | Corporation. </span>
+        <span id="secondSpan">
+          <ul id="ulHeader">
+            <li class="liHeader">
+              <a href="#">À Propos</a>
+            </li>
+
+            <li class="liHeader">
+              <a href="#">Projets</a>
+            </li>
+
+            <li class="liHeader">
+              <a href="#">Contacte</a>
+            </li>
+          </ul>
+        </span>
+      </span>
+    </nav>
+  );
+};
