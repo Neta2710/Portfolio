b5d1454275cdd248e41e7f081ed0b5efd7db6085
Neta <chetritnetanael@gmail.com>
2023-06-01 19:15:01 +0200

    update neta v3

--- src/components/TopPresentation/TopPresentation.jsx
+++ src/components/TopPresentation/TopPresentation.jsx
@@ -1,27 +1,28 @@
 import React, { useEffect, useState } from "react";
+import Septre  from "./Septre/Septre";
 import "./TopPresentation.css";
 
 const TopPresentation = () => {
   return (
-    <div className="top-presentation">
-      <div>
-        <divc className="septre">
-          <div className="cercleViolet"></div>
-          <div className="tigeFade"></div>
-        </divc>
-        <h1>
-          <h1 id="title">
-           Bonjour, nous sommes 
-            <span id="cacheTech"> CacheTech</span>
+    <section className="top-presentation">
+      <div id="flexPosition">
+          <Septre id="Septre"/>
+          <span id="textTop">
+          <h1>
+            <h1 id="title">
+              Hi, I'm 
+              <span id="cacheTech"> CacheTech</span>
+            </h1>
+            <p id="presentationTop">
+              Nous sommes une entreprise de création de site web et
+              d'application,
+              <br />
+              Cela grâce à nos attribus de développer FullStack
+            </p>
           </h1>
-          <p id="presentationTop">
-            Nous sommes une entreprise de création de site web et d'application,
-            <br />
-            Cela grâce à nos attribus de développer FullStack
-          </p>
-        </h1>
+          </span>
       </div>
-    </div>
+    </section>
   );
 };
 
